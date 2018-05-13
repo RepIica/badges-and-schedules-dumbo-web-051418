e2fba4e3e518e8a377c8b1116708cf3f780be2f9
Sammy Akharaz <ketrixz@gmail.com>
2018-05-13 21:12:05 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,7 +11,6 @@ def batch_badge_creator(names)
 end
 
 def assign_rooms(names)
-  room_num = 1
   name.each_with_index.map do |attendee, index|
     "Hello, #{attendee}! You'll be assigned to room #{index+1}!"
   end
